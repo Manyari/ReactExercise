9ea3a675b03378b5d54ea89fd34264aeaaf745d7
Manyari <Iraynam.1768@gmail.com>
2020-10-25 21:43:26 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,141 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Toast from 'react-bootstrap/Toast';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-
+import { Form, Container, Button, Row, Modal } from 'react-bootstrap';
 import './App.css';
 
-const ExampleToast = ({ children }) => {
-  const [show, toggleShow] = useState(true);
+import { initialState } from './constants';
+import { actions } from './actions';
+import { reducer } from './reducer';
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [Name, setName] = useState(false);
+  const [Email, setEmail] = useState(false);
+  const [Kilometers, setKilometers] = useState(false);
+  const [Dates, setForm] = useState(false);
+
+  const saveDate = (e) => {
+    let { name, value } = e.target;
+
+    if (value.length === 0) {
+      setForm(true);
+    } else {
+      setForm(false);
+    }
+    if (value.length > 0) {
+      if (name === 'name') {
+        if (value.length === 0) {
+          setName(true);
+        } else {
+          setName(false);
+        }
+      }
+
+      if (name === 'email') {
+        if (value.length === 0) {
+          setEmail(true);
+        } else {
+          setEmail(false);
+        }
+      }
+
+      if (name === 'kilometers') {
+        if (value.length === 0) {
+          setKilometers(true);
+        } else {
+          setKilometers(false);
+        }
+      }
+    }
+    dispatch({ type: actions.saveDate, name, payload: value });
+  };
+  
+  const showModal = () => {
+    dispatch({ type: actions.showModal });
+  };
+  const hideModal = () => {
+    dispatch({ type: actions.toggleModal });
+  };
 
   return (
-    <>
-      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
-      <Toast show={show} onClose={() => toggleShow(false)}>
-        <Toast.Header>
-          <strong className="mr-auto">React-Bootstrap</strong>
-        </Toast.Header>
-        <Toast.Body>{children}</Toast.Body>
-      </Toast>
-    </>
+    <Container className='header'>
+      <Row className='justify-content-center'>
+        <h1>Exercise Level</h1>
+      </Row>
+      <Form>
+        <Form.Group controlId='formBasicEmail'>
+          {Dates && <p className='required'>Porfavor llene todos los campos</p>}
+          {Name && (
+            <p className='required' style={{ color: 'red', fontSize: '11px' }}>
+              Name is Required
+            </p>
+          )}
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            name='name'
+            type='name'
+            onChange={saveDate}
+            value={state.date.name}
+            param={Name}
+            placeholder='Enter name'
+          />
+        </Form.Group>
+
+        <Form.Group controlId='formBasicPassword'>
+          {Email && (
+            <p className='required' style={{ color: 'red', fontSize: '11px' }}>
+              Email is required
+            </p>
+          )}
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            name='email'
+            type='email'
+            onChange={saveDate}
+            value={state.date.email}
+            param={Email}
+            placeholder='Enter email'
+          />
+        </Form.Group>
+
+        <Form.Group controlId='formBasicKilometer'>
+          {Kilometers && (
+            <p className='required' style={{ color: 'red', fontSize: '11px' }}>
+              Kilometers is required
+            </p>
+          )}
+          <Form.Label>Kilometers</Form.Label>
+          <Form.Control
+            name='kilometers'
+            type='number'
+            onChange={saveDate}
+            value={state.date.kilometers}
+            param={Kilometers}
+            placeholder='Enter how many kilometers you walk per week'
+          />
+        </Form.Group>
+      </Form>
+
+      <Button onClick={() => showModal()}>
+        <span className='pcoded-micon'>Submit</span>
+      </Button>
+
+      <Modal show={state.showModal} onHide={hideModal}>
+        <Modal.Header closeButton></Modal.Header>
+        <Modal.Body>
+          {state.date.name}
+          {state.date.kilometers >= 4
+            ? 'Congratulations'
+            : 'You must walk more'}
+          {state.date.email}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='primary' onClick={hideModal}>
+            Aceptar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Container>
   );
 };
-
-const App = () => (
-  <Container className="p-3">
-    <Jumbotron>
-      <h1 className="header">Welcome To React-Bootstrap</h1>
-      <ExampleToast>
-        We now have Toasts
-        <span role="img" aria-label="tada">
-          🎉
-        </span>
-      </ExampleToast>
-    </Jumbotron>
-  </Container>
-);
-
 export default App;
